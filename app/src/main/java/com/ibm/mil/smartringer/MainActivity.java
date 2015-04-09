15f0121d7f65851be74ccc209823e1fcd9f3c746
John Petitto <john.petitto@gmail.com>
2015-04-09 00:55:41 -0500

    Save settings option

--- app/src/main/java/com/ibm/mil/smartringer/MainActivity.java
+++ app/src/main/java/com/ibm/mil/smartringer/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getName();
@@ -14,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
     private NoiseMeter mNoiseMeter;
     private Handler mHandler;
     private Runnable mRunnable;
+    private SensitivityLevel mSensitivityLevel = SensitivityLevel.MED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,18 +56,31 @@ public class MainActivity extends ActionBarActivity {
         switch (view.getId()) {
             case R.id.low_button:
                 Log.i(TAG, "Low sensitivity");
+                mSensitivityLevel = SensitivityLevel.LOW;
                 break;
             case R.id.med_button:
                 Log.i(TAG, "Medium sensitivity");
+                mSensitivityLevel = SensitivityLevel.MED;
                 break;
             case R.id.high_button:
                 Log.i(TAG, "High sensitivity");
+                mSensitivityLevel = SensitivityLevel.HIGH;
                 break;
         }
     }
 
-    public void onNoiseTestClicked(View view) {
+    public void onSaveClicked(View view) {
+        Toast.makeText(this, R.string.settings_saved, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onTestClicked(View view) {
         Log.i(TAG, "Test noise sensitivity...");
+    }
+
+    private enum SensitivityLevel {
+        LOW,
+        MED,
+        HIGH
     }
 
 }
