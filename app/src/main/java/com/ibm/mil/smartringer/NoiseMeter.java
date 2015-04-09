1dfa573ae800c50a125fa29f731740b9578461a4
John Petitto <john.petitto@gmail.com>
2015-04-08 23:03:14 -0500

    Monitor noise level through mic

---
+++ app/src/main/java/com/ibm/mil/smartringer/NoiseMeter.java
@@ -0,0 +1,43 @@
+package com.ibm.mil.smartringer;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+public class NoiseMeter {
+    private static final int SAMPLE_RATE = 8000;
+    private static final int MIN_BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE,
+            AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+
+    private AudioRecord mAudioRecord;
+
+    public void start() {
+        mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE,
+                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, MIN_BUFFER_SIZE);
+        mAudioRecord.startRecording();
+    }
+
+    public void stop() {
+        if (mAudioRecord != null) {
+            mAudioRecord.stop();
+        }
+    }
+
+    public double getMaxAmplitude() {
+        short[] buffer = new short[MIN_BUFFER_SIZE];
+
+        if (mAudioRecord != null) {
+            mAudioRecord.read(buffer, 0, MIN_BUFFER_SIZE);
+        }
+
+        int max = 0;
+        for (short s : buffer) {
+            int abs = Math.abs(s);
+            if (abs > max) {
+                max = abs;
+            }
+        }
+
+        return max;
+    }
+}
