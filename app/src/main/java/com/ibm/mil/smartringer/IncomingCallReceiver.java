bd93cc571b485b8fe019e8d683e5e8c26558ab2e
John Petitto <johnpetitto@us.ibm.com>
2015-04-18 23:39:59 -0500

    Create BroadcastReceiver for incoming calls

---
+++ app/src/main/java/com/ibm/mil/smartringer/IncomingCallReceiver.java
@@ -0,0 +1,21 @@
+package com.ibm.mil.smartringer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class IncomingCallReceiver extends BroadcastReceiver {
+    private static final String TAG = IncomingCallReceiver.class.getName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String callState = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+        if (callState.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+            Log.i(TAG, "Incoming phone call detected");
+            // start service to adjust ringer volume for incoming call
+        }
+    }
+
+}
